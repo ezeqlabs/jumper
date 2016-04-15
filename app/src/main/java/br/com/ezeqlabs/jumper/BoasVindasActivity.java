0dc5fd549ea32231dd68ff14f530a3ac24ccdb64
Kako Botasso <kakodev@gmail.com>
2016-04-15 13:54:33 -0300

    Adicionando publicidade na tela inicial do site

--- app/src/main/java/br/com/ezeqlabs/jumper/BoasVindasActivity.java
+++ app/src/main/java/br/com/ezeqlabs/jumper/BoasVindasActivity.java
@@ -7,12 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class BoasVindasActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boas_vindas);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         Button jogar = (Button) findViewById(R.id.menu_principal_jogar);
         jogar.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/br/com/ezeqlabs/jumper/helpers/Constantes.java
+++ app/src/main/java/br/com/ezeqlabs/jumper/helpers/Constantes.java
@@ -2,4 +2,5 @@ package br.com.ezeqlabs.jumper.helpers;
 
 public class Constantes {
     public static final String ID_INTERSTITIAL_AD = "ca-app-pub-8736689435854328/9048907997";
+    public static final String ID_BANNER_HOME_AD = "ca-app-pub-8736689435854328/4280631196";
 }
