d77dddddb4300e7e8f2a5a9c808770d76f9d15b7
Kako Botasso <kakodev@gmail.com>
2016-04-22 23:06:26 -0300

    Melhorando cor das sombras dos textos

--- app/src/main/java/br/com/ezeqlabs/jumper/engine/Cores.java
+++ app/src/main/java/br/com/ezeqlabs/jumper/engine/Cores.java
@@ -21,7 +21,7 @@ public class Cores {
         branco.setColor(0xFFFFFFFF);
         branco.setTextSize(80);
         branco.setTypeface(Typeface.DEFAULT_BOLD);
-        branco.setShadowLayer(3, 5, 5, 0xFF000000);
+        branco.setShadowLayer(3, 5, 5, 0xFF4682B4);
         return branco;
     }
 
@@ -30,25 +30,25 @@ public class Cores {
         vermelho.setColor(0xFFCF0505);
         vermelho.setTextSize(100);
         vermelho.setTypeface(Typeface.DEFAULT_BOLD);
-        vermelho.setShadowLayer(2, 3, 3, 0xFF000000);
+        vermelho.setShadowLayer(2, 3, 3, 0xFF4682B4);
         return vermelho;
     }
 
     public static Paint getCorPontuacaoDoGameOver(){
         Paint marrom = new Paint();
-        marrom.setColor(0xFFC36510);
+        marrom.setColor(0xFFFFA500);
         marrom.setTextSize(70);
         marrom.setTypeface(Typeface.DEFAULT_BOLD);
-        marrom.setShadowLayer(2, 3, 3, 0xFF000000);
+        marrom.setShadowLayer(2, 3, 3, 0xFF4682B4);
         return marrom;
     }
 
     public static Paint getCorNovamenteDoGameOver(){
         Paint marrom = new Paint();
-        marrom.setColor(0xFFC36510);
+        marrom.setColor(0xFFFFA500);
         marrom.setTextSize(50);
         marrom.setTypeface(Typeface.DEFAULT_BOLD);
-        marrom.setShadowLayer(2, 3, 3, 0xFF000000);
+        marrom.setShadowLayer(2, 3, 3, 0xFF4682B4);
         return marrom;
     }
 }
