b97660c6442cd651a18849782d0984a2e7c27e38
Kako Botasso <kakodev@gmail.com>
2016-09-07 18:36:07 -0300

    Adicionando animação a tela inicial

--- app/src/main/java/br/com/ezeqlabs/jumper/EzeqlabsActivity.java
+++ app/src/main/java/br/com/ezeqlabs/jumper/EzeqlabsActivity.java
@@ -1,19 +1,48 @@
 package br.com.ezeqlabs.jumper;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.RelativeLayout;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class EzeqlabsActivity extends Activity {
+    private Scene scene;
+    private Transition transition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ezeqlabs);
 
+        if(Build.VERSION.SDK_INT >= 21){
+            setContentView(R.layout.activity_ezeqlabs_init);
+            RelativeLayout baseLayout = (RelativeLayout) findViewById(R.id.ezeq_layout);
+
+            scene = Scene.getSceneForLayout(baseLayout, R.layout.activity_ezeqlabs, this);
+
+            transition = new AutoTransition();
+            transition.setDuration(1000);
+            transition.setInterpolator(new AccelerateDecelerateInterpolator());
+            TransitionManager.go(scene, transition);
+        }else{
+            setContentView(R.layout.activity_ezeqlabs);
+        }
+
+        mudaTela();
+
+    }
+
+    private void mudaTela(){
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -22,6 +51,6 @@ public class EzeqlabsActivity extends Activity {
                 startActivity(lugares);
                 finish();
             }
-        }, 2000);
+        }, 3000);
     }
 }
